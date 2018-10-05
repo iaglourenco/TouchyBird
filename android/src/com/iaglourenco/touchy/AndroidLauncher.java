3ffe6d01d3e8dca5fb77725b271232d1e6159559
Iago Lourenço <iagojlourenco@gmail.com>
2018-10-05 13:27:11 -0300

    Added libgdx files

---
+++ android/src/com/iaglourenco/touchy/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.iaglourenco.touchy;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.iaglourenco.touchy.Touchy;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Touchy(), config);
+	}
+}
